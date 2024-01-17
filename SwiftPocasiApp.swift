bf82655bb0e65cfecf826beb6d1d80e3dab5d019
Šimon Vepřek <81298182+SimonCodess@users.noreply.github.com>
2024-01-17 15:37:40 +0100

    Create SwiftPocasiApp.swift

---
+++ SwiftPocasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPocasiApp.swift
+//  SwiftPocasi
+//
+//  Created by SimonV on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
